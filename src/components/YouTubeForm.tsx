20709d8422b8e31f42a05e74ca4c1feeee6aaf69
Cesar Mejia <cmejia@gmail.com>
2023-04-05 09:22:47 +0700

    first commit

---
+++ src/components/YouTubeForm.tsx
@@ -0,0 +1,18 @@
+export const YouTubeForm = () => {
+  return (
+    <div>
+      <form>
+        <label htmlFor='username'>Username</label>
+        <input type='text' name='username' id='username' />
+
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email' id='email' />
+
+        <label htmlFor='channel'>Channel</label>
+        <input type='text' name='channel' id='channel' />
+
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
