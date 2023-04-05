883c5298e4818afd21e0bfc0824f392ee3f0ed77
Cesar Mejia <cmejia@gmail.com>
2023-04-05 13:46:42 +0700

    lesson 3

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,17 +1,39 @@
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+type Inputs = {
+  username: string
+  email: string
+  channel: string
+}
+
 export const YouTubeForm = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+  console.log(watch('username'))
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor='username'>Username</label>
-        <input type='text' name='username' id='username' />
+        <input type='text' id='username' {...register('username', { required: true })} />
+        {errors.username && <span>This field is required</span>}
 
         <label htmlFor='email'>Email</label>
-        <input type='email' name='email' id='email' />
+        <input type='email' id='email' {...register('email', { required: true })} />
+        {errors.email && <span>This field is required</span>}
 
         <label htmlFor='channel'>Channel</label>
-        <input type='text' name='channel' id='channel' />
-
-        <button>Submit</button>
+        <input type='text' id='channel' {...register('channel', { required: true })} />
+        {errors.channel && <span>This field is required</span>}
+        <div>
+          <button>Submit</button>
+        </div>
       </form>
     </div>
   )
